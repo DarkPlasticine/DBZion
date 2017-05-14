eef9e6f1bb59967bc4afbd20eab77e7102ab4abf
Garik <maximgagarinov93@gmail.com>
2017-05-14 21:29:47 +0300

    Использование ObservableCollection в OrderService.

--- DBZion.BLL/Interfaces/IOrderService.cs
+++ DBZion.BLL/Interfaces/IOrderService.cs
@@ -1,6 +1,7 @@
 ﻿using DBZion.DAL.Entities;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
@@ -10,14 +11,15 @@ namespace DBZion.BLL.Interfaces
     {
         void AddOrder(string userSurname, string userFirstName, string userMiddleName, string userPhoneNumber, int receiptId, string receiptType, string serviceType, int price, DateTime orderDate, string description, string note, bool isActive, bool isReady, bool call, string worker);
         void UpdateOrder(int id, string userSurname, string userFirstName, string userMiddleName, string userPhoneNumber, int receiptId, string receiptType, string serviceType, int price, string description, string note, bool isActive, bool isReady, bool call, string worker);
-        List<Order> GetOrders();
-        List<Order> GetOrders(Func<Order, bool> predicate);
+        ObservableCollection<Order> GetOrders();
+        ObservableCollection<Order> GetOrders(Func<Order, bool> predicate);
+        Task<ObservableCollection<Order>> GetOrdersAsync();
+        Task<ObservableCollection<Order>> GetOrdersAsync(Expression<Func<Order, bool>> predicate);
         List<X> GetFieldValues<X>(Func<Order, X> selector);
         List<X> GetFieldValues<X>(Func<Order, bool> predicate, Func<Order, X> selector);
         Task<List<X>> GetFieldValuesAsync<X>(Expression<Func<Order, X>> selector);
         Task<List<X>> GetFieldValuesAsync<X>(Expression<Func<Order, bool>> predicate, Expression<Func<Order, X>> selector);
-        Task<List<Order>> GetOrdersAsync();
-        Task<List<Order>> GetOrdersAsync(Expression<Func<Order, bool>> predicate);
+
 
         void AddUser(string surname, string firstName, string middleName, string phoneNumber);
         User FindUser(int id);
