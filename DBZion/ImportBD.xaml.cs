3b33e30ef07dfff0e1e9492c6e2af1c5b5f1d6ca
Shpizpurgen <User@SHPIZPURGEN-PC>
2017-08-10 19:21:59 +0300

    фикс полключения к бд

--- DBZion/ImportBD.xaml.cs
+++ DBZion/ImportBD.xaml.cs
@@ -17,6 +17,7 @@ using System.Xml.Serialization;
 using DBZion.BLL.Services;
 using DBZion.DAL.Entities;
 using MahApps.Metro.Controls;
+using System.ComponentModel;
 
 namespace DBZion
 {
@@ -27,40 +28,33 @@ namespace DBZion
     {
         protected internal OrderService service = null;
         List<OldOrder> oldOrders = new List<OldOrder>();
+        BackgroundWorker backgroundWorker;
         int current = 0;
         int full = 0;
         string count = @"Импортированно: {0}/{1}";
         public ImportBD()
         {
             InitializeComponent();
-            service = new OrderService(@"Data Source=Shpizpurgen-PC\SQLExpress;Initial Catalog=DataBaseZion;Integrated Security=True");
+            service = new OrderService("DbConnection");
+
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += new DoWorkEventHandler(BackgroundWorker_DoWork);
+            backgroundWorker.ProgressChanged += new ProgressChangedEventHandler(BackgroundWorker_ProgressChanged);
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.WorkerSupportsCancellation = true;
+            backgroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(BackgroundWorker_RunWorkerCompleted);
         }
 
-        private void btnImport_Click(object sender, RoutedEventArgs e)
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
         {
-            // List<OldOrder> oldOrders = new List<OldOrder>();
-            //oldOrders.Clear();
-            //try
-            //{
-            //    XmlSerializer formatter = new XmlSerializer(typeof(List<OldOrder>));
-
-            //    using (FileStream fs = new FileStream(txbPath.Text, FileMode.OpenOrCreate, FileAccess.Read))
-            //    {
-            //        oldOrders = (List<OldOrder>)formatter.Deserialize(fs);
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(ex.Message);
-            //}
-
             foreach (OldOrder o in oldOrders)
             {
+                current++;
                 string[] u = o.FullName.Split(new char[] { ' ', '.' }, StringSplitOptions.RemoveEmptyEntries);
                 //if (o.PhoneNumber == null)
                 //    o.PhoneNumber = "8(800)555-3535";
 
-                if (o.ServiceType == "" )
+                if (o.ServiceType == "")
                     o.ServiceType = o.ReceiptType;
                 if (o.ServiceType.Length > 20)
                 {
@@ -91,7 +85,7 @@ namespace DBZion
                     },
                     Worker = o.Worker
                 };
-               
+
                 switch (u.Count())
                 {
                     case 1: _order.User.Surname = u[0]; _order.User.FirstName = "Аноним"; _order.User.MiddleName = "Анонимович"; break;
@@ -101,9 +95,91 @@ namespace DBZion
                 }
 
                 service.AddOrder(_order);
-            }
 
+                backgroundWorker.ReportProgress(current);
+            }
+        }
+
+        private void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            lblProgress.Content = String.Format(count, e.ProgressPercentage, full);
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
             MessageBox.Show("OK");
+        }
+
+        private void btnImport_Click(object sender, RoutedEventArgs e)
+        {
+            // List<OldOrder> oldOrders = new List<OldOrder>();
+            //oldOrders.Clear();
+            //try
+            //{
+            //    XmlSerializer formatter = new XmlSerializer(typeof(List<OldOrder>));
+
+            //    using (FileStream fs = new FileStream(txbPath.Text, FileMode.OpenOrCreate, FileAccess.Read))
+            //    {
+            //        oldOrders = (List<OldOrder>)formatter.Deserialize(fs);
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+
+            //foreach (OldOrder o in oldOrders)
+            //{
+            //    string[] u = o.FullName.Split(new char[] { ' ', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            //    //if (o.PhoneNumber == null)
+            //    //    o.PhoneNumber = "8(800)555-3535";
+
+            //    if (o.ServiceType == "" )
+            //        o.ServiceType = o.ReceiptType;
+            //    if (o.ServiceType.Length > 20)
+            //    {
+            //        o.Description = o.Description + "\n" + o.ServiceType;
+            //        o.ServiceType = o.ReceiptType;
+            //    }
+            //    if (o.Description == "" || o.Description == null)
+            //        o.Description = "no description";
+
+            //    if (o.Worker == "" || o.Worker == null)
+            //        o.Worker = "admin";
+
+            //    Order _order = new Order()
+            //    {
+            //        Call = o.Call,
+            //        Description = o.Description,
+            //        IsActive = o.IsActive,
+            //        IsReady = o.IsReady,
+            //        Note = o.Note,
+            //        OrderDate = o.OrderDate,
+            //        Price = o.Price,
+            //        ReceiptId = o.ReceiptID,
+            //        ReceiptType = o.ReceiptType,
+            //        ServiceType = o.ServiceType,
+            //        User = new User()
+            //        {
+            //            PhoneNumber = o.PhoneNumber
+            //        },
+            //        Worker = o.Worker
+            //    };
+               
+            //    switch (u.Count())
+            //    {
+            //        case 1: _order.User.Surname = u[0]; _order.User.FirstName = "Аноним"; _order.User.MiddleName = "Анонимович"; break;
+            //        case 2: _order.User.FirstName = u[1]; _order.User.Surname = u[0]; _order.User.MiddleName = "Анонимович"; break;
+            //        case 3: _order.User.FirstName = u[1]; _order.User.Surname = u[0]; _order.User.MiddleName = u[2]; break;
+            //        default: _order.User.FirstName = "Аноним"; _order.User.Surname = "Анонимов"; _order.User.MiddleName = "Анонимович"; break;
+            //    }
+
+            //    service.AddOrder(_order);
+            //}
+
+            //MessageBox.Show("OK");
+
+            backgroundWorker.RunWorkerAsync();
         }
 
         private void btnPath_Click(object sender, RoutedEventArgs e)
@@ -171,6 +247,11 @@ namespace DBZion
                 Worker = worker;
                 PhoneNumber = phone;
             }
+        }
+
+        private void MetroWindow_Closing(object sender, CancelEventArgs e)
+        {
+            service.Dispose();
         }
     }
 }
